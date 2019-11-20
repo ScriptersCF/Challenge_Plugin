98f176793533367dd5e440f57ea6738949bf14e2
Josh <me@joshl.io>
2019-11-20 16:44:09 +0000

    Create Tutorial.lua

---
+++ Tutorial.lua
@@ -0,0 +1,21 @@
+--[[
+	
+	This is a tutorial challenge.
+	
+	- In this challenge, you are being instructed to multiply the two numbers
+	together.
+	- These numbers are passed as arguments when you test your solution.
+	- In this case, those numbers are being represented by the variables "x" and "y".
+	- It is your job to write a script that can calculate the solution based on
+	the input given.
+	- In the example below that has been written for you, the result is calculated
+	by adding the two values together rather than multiplying them.
+	- You need to edit the example so that it multiplies the values instead.
+	- Once you have finished, hit "Test" above the test cases to check your answer!
+	
+--]]
+
+return function(x, y)
+	local Result = x + y
+	return Result
+end
